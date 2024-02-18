48c3e0b9986999a0ec0a5df5b0d7a0260ec90cc4
micriteo <teodorfolea@yahoo.com>
2024-02-18 16:46:25 +0100

    .NET Maui initiliazation  + MVVM structure

---
+++ TripBuddy/TripBuddy/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripBuddy.ViewModel
+{
+    internal class MainPageViewModel
+    {
+        public int Count { get; private set; }
+
+        public MainPageViewModel()
+        {
+            Count = 0;
+        }
+
+        public void IncrementCount()
+        {
+            Count++;
+        }
+    }
+}
