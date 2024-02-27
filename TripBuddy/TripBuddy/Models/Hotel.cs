22dc93616611a72e2f699e8b6933eedfcd2f73a1
micriteo <teodorfolea@yahoo.com>
2024-02-27 11:24:13 +0100

    Base Classes created

---
+++ TripBuddy/TripBuddy/Models/Hotel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripBuddy.Models
+{
+    internal class Hotel
+    {
+    }
+}
---
+++ TripBuddy/TripBuddy/Models/Trip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripBuddy.Models
+{
+    internal class Trip
+    {
+
+        // This class is used to store information about a trip
+
+        public double TotalPrice { get; set; }
+        public HashSet<LocationStop> Stops { get; set; }
+
+
+        // Constructor
+        public Trip()
+        {
+            TotalPrice = 0;
+            Stops = new HashSet<LocationStop>();
+        }
+
+        public void calculateTotalPrice()
+        {
+            double sum = 0;
+            foreach (LocationStop stop in Stops)
+            {
+                sum += stop.Hotel.Price;
+            }
+
+            this.TotalPrice = sum;
+        }
+    }
+}
