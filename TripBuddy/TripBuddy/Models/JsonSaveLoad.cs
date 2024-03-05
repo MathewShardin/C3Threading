bfaf1ce9e3dafed4076148af835454bb6c2430c3
Dimitri Vastenhout <dimitri.vastenhout@gmail.com>
2024-03-05 15:44:25 +0100

    made class base

---
+++ TripBuddy/TripBuddy/Models/JsonSaveLoad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+namespace TripBuddy.Models
+{
+    internal class JsonSaveLoad
+    {
+        string creationDate { get; }
+        Trip trip = new Trip();
+
+        public JsonSaveLoad(Trip trip)
+        {
+            this.trip = trip;
+        }
+
+        public async void makeJSON()
+        {
+                        
+        }
+    }
+}
