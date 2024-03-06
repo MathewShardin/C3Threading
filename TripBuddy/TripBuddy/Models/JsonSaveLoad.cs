2a7e1a8461b76d5fbc591bff9042532fa87c6aa0
Dimitri Vastenhout <dimitri.vastenhout@gmail.com>
2024-03-06 12:29:14 +0100

    made the file creation doesnt fully work yet

--- TripBuddy/TripBuddy/Models/JsonSaveLoad.cs
+++ TripBuddy/TripBuddy/Models/JsonSaveLoad.cs
@@ -12,24 +12,45 @@ namespace TripBuddy.Models
 {
     internal class JsonSaveLoad
     {
-        string creationDate { get; }
+        DateTime creationDate { get; }
         Trip trip = new Trip();
 
         public JsonSaveLoad(Trip trip)
         {
             this.trip = trip;
+            creationDate = DateTime.Now;
         }
 
+        //turns the trip and date into a Json string asynchronously
         public async void MakeJsonAsync()
         {
+            //makes json string
             string jsonString = JsonSerializer.Serialize(trip);
-            Debug.WriteLine(jsonString);
+
+            //make the file name of the first hotel
+            string fileName = trip.Stops.ToList().First().Hotel.Name;
+
+            //make file via filestream
+            await using FileStream createStream = File.Create(fileName);
+
+            //serialize the json into the file
+            await JsonSerializer.SerializeAsync(createStream, jsonString);
+
         }
 
         public void MakeJson()
         {
+            //makes json string
             string jsonString = JsonSerializer.Serialize(trip);
-            Debug.WriteLine(jsonString);
+
+            //make the file name of the first hotel
+            string fileName = trip.Stops.ToList().First().Hotel.Name;
+
+            //make file via filestream
+            using FileStream createStream = File.Create(fileName);
+
+            //serialize the json into the file
+            JsonSerializer.SerializeAsync(createStream, jsonString);
         }
     }
 }
