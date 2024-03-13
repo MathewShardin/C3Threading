c723642fcdef99b92414a6e27fed21d68599651e
Dimitri Vastenhout <dimitri.vastenhout@gmail.com>
2024-03-13 10:57:09 +0100

    added json load and save class to Models

---
+++ TripBuddy/TripBuddy/Models/JsonSaveLoad.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Diagnostics;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+
+namespace TripBuddy.Models
+{
+    internal class JsonSaveLoad
+    {
+        DateTime creationDate { get; }
+        Trip trip = new Trip();
+
+        public JsonSaveLoad(Trip trip)
+        {
+            this.trip = trip;
+            creationDate = DateTime.Now;
+        }
+
+        //turns the trip and date into a Json string asynchronously
+        public async void MakeJsonAsync()
+        {
+            //makes json string
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(trip, new JsonSerializerOptions
+            {
+                //This will format the JSON with indentation
+                WriteIndented = true
+            }); ;
+
+            //make the file name of the first hotel
+            string fileName = trip.Stops.ToList().First().Hotel.Name + ".json";
+
+            // Get dynamic file path to saves folder
+            string workingDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\..\\"));
+            string filePath = Path.Combine(workingDirectory, "Saves", fileName);
+
+            //make file via filestream and add the text
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public void MakeJson()
+        {
+            //makes json string
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(trip, new JsonSerializerOptions
+            {
+                //This will format the JSON with indentation
+                WriteIndented = true
+            }); ;
+
+            //make the file name of the first hotel
+            string fileName = trip.Stops.ToList().First().Hotel.Name + ".json";
+
+            // Get dynamic file path to saves folder
+            string workingDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\..\\"));
+            string filePath = Path.Combine(workingDirectory, "Saves", fileName);
+
+            //make file via filestream and add the text
+            File.WriteAllTextAsync(filePath, jsonString);
+        }
+
+        public Trip loadJson()
+        {
+            //get file
+            string fileName = "hotelplace.json";
+            //Get dynamic file path to saves folder
+            string workingDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\..\\"));
+            string filePath = Path.Combine(workingDirectory, "Saves", fileName);
+
+            //read file
+            var jsonFile = JObject.Parse(File.ReadAllText(filePath));
+            string jsonString = jsonFile.ToString();
+            Trip trip = JsonConvert.DeserializeObject<Trip>(jsonString);
+            return trip;
+        }
+    }
+}
