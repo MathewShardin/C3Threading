1009692ce6c31d306be5e20de8e535d794ec5f01
Dimitri Vastenhout <dimitri.vastenhout@student.nhlstenden.com>
2024-03-13 19:32:28 +0100

    made function (not tested yet)

---
+++ TripBuddy/TripBuddy/Models/Search.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripBuddy.Models
+{
+    static class Search
+    {
+        public static async Task<List<Hotel>> SearchHotelsAsync(List<Hotel> hotels, string searchQuery)
+        {
+            //if empty string do nothing
+            if (hotels == null)
+            {
+                return new List<Hotel>();
+            }
+
+            //use PLINQ to search hotels in parallel
+            var searchResult = await Task.Run(() =>
+            {
+                return hotels.AsParallel()
+                    .Where(hotel => hotel.Name.ToLower().Contains(searchQuery))
+                    .ToList();
+            });
+
+            return searchResult;
+        }
+    }
+}
--- TripBuddy/TripBuddy/Views/MainPage.xaml.cs
+++ TripBuddy/TripBuddy/Views/MainPage.xaml.cs
@@ -7,6 +7,7 @@ namespace TripBuddy
         public MainPage()
         {
             InitializeComponent();
+
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
