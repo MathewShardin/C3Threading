dd534d7af4f4de30dff7c924171e8b9a5242023e
Dimitri Vastenhout <dimitri.vastenhout@gmail.com>
2024-03-13 10:36:24 +0100

    removed testing code getting ready for merge

--- TripBuddy/TripBuddy/Views/MainPage.xaml.cs
+++ TripBuddy/TripBuddy/Views/MainPage.xaml.cs
@@ -11,27 +11,7 @@ namespace TripBuddy
 
         private async void OnCounterClicked(object sender, EventArgs e)
         {
-            var file = CsvAccessor.ReadCsvFile();
-            CounterBtn.Text = $"text changed";
-
-            //Making trip
-            Trip trip = new Trip();
-            //Making city
-            City city = new City("towscity");
-            //Making Hotel
-            Hotel hotel = new Hotel("hotelplace", 1000, city, "nice place", 1);
-            //Making Location Stop
-            LocationStop stop = new LocationStop(hotel, DateTime.Now);
-            //add stop to trip
-            trip.AddStop(stop);
-
-            JsonSaveLoad json = new JsonSaveLoad(trip);
-
-            //json.MakeJsonAsync();
-
-            //json.loadJson();
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            
         }
     }
 }
