f861c8cedce07354323961e2106649fc246c35ec
Corvin Wittmaack <corvin.wittmaack@student.nhlstenden.com>
2024-03-20 13:17:46 +0100

    Put all hotels on the ui

--- TripBuddy/TripBuddy/ViewModel/MainPageViewModel.cs
+++ TripBuddy/TripBuddy/ViewModel/MainPageViewModel.cs
@@ -21,11 +21,11 @@ namespace TripBuddy.ViewModel
 
         public MainPageViewModel()
         {
-            this.Hotels = new ObservableCollection<Hotel>();
-            this.Count = 0;
-            this.City = new City("City", "Country","010002000");
-            this.Hotel = new Hotel("Dimis House", 100, City, "good", 1);
-            this.Hotels.Add(this.Hotel);
+            DataStore dStore = new DataStore();
+
+            dStore.ParseFromCsv();
+
+            Hotels = new ObservableCollection<Hotel>(dStore.HotelCatalogue);
         }
     }
 }
